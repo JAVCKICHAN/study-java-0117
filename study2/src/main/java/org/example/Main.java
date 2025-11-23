b948388108368008962e55637a2ee72b602b8a53
JAVCKICHAN <iluvderry@gmail.com>
2025-11-23 16:33:53 +0900

    copy image from external source

--- study2/src/main/java/org/example/Main.java
+++ study2/src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args)throws Exception {
+
+        //[외부 이미지를 복사해보기]
+
+        String path = "https://image.coupangcdn.com/image/vendor_inventory/image_audit/stage/manual/25b112daf919aff8f577533c4d1083b9382b78a0ae2d7d4893ac1c6858cf_1762234162994.jpg";
+        URL url = new URL(path);
+
+        //in 빨대 만들기(읽기)
+        InputStream in = url.openStream();
+        //out 빨대 만들기(쓰기)
+        OutputStream out = new FileOutputStream("/Users/kimda/study-java/image/copy.jpg");
+
+        //빨아들이기 - read
+        while(true){ //많은 byte를 읽어야 하기 때문에? while
+
+            int data = in.read();// 1byte를 읽음.
+            //read가 추상 메서드임. 함수형으로 설계되 int값으로 뭔가를 줘야함.
+            //내부적으로 + 255를 함 -> 음수가 나올 수 없게 설계
+
+            //System.out.println(data);
+
+            //더 이상 읽을게 없을 경우 -1을 반환.(=함수형)
+            if(data == -1){
+                break;
+            }
+            out.write(data);
         }
+
+        in.close(); //빨때를 뽑는다.
+        out.close();
+
+
+
     }
 }
